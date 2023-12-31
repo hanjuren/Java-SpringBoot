6797ab9f227bc4c2e465d9c171f7ef2914e61fdf
hanjuren <juren528@gmail.com>
2023-12-31 20:37:49 +0900

    component scan

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                classes = Configuration.class
+        ),
+        basePackages = "hello.core"
+)
+public class AutoAppConfig {
+
+//        @Bean(name = "memoryMemberRepository")
+//        public MemberRepository memberRepository() {
+//                return new MemoryMemberRepository();
+//        }
+}
