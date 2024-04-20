346cdf6f9974fdaa8d9dff829ace8767cb435f64
hanjuren <juren528@gmail.com>
2024-04-20 15:40:11 +0900

    Object Array

---
+++ java-mid/java/src/lang/poly/ObjectPolyExample2.java
@@ -0,0 +1,18 @@
+package lang.poly;
+
+public class ObjectPolyExample2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+        Object obj = new Object();
+
+        Object[] objectArray = {dog, car, obj};
+
+        size(objectArray);
+    }
+
+    private static void size(Object[] objectArray) {
+        System.out.println("Size of objectArray: " + objectArray.length);
+    }
+}
