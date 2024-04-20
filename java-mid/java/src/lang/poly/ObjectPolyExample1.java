4766767f71f82216e682074e66b3a48424f1f15b
hanjuren <juren528@gmail.com>
2024-04-20 14:30:43 +0900

    Object Polymorphism

---
+++ java-mid/java/src/lang/poly/ObjectPolyExample1.java
@@ -0,0 +1,26 @@
+package lang.poly;
+
+public class ObjectPolyExample1 {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        Dog dog = new Dog();
+
+        action(car);
+        action(dog);
+    }
+
+    public static void action(Object obj) {
+        // obj.sound() // Compile Error
+        // obj.move() // Compile Error
+
+        // down casting
+        if (obj instanceof Car car) {
+            car.move();
+        } else if (obj instanceof Dog dog) {
+            dog.sound();
+        } else {
+            System.out.println("지원하지 않는 객체입니다.");
+        }
+    }
+}
