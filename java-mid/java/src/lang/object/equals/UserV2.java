834be883b69b68daab722cd67fb99666447f1afc
hanjuren <juren528@gmail.com>
2024-04-23 21:26:52 +0900

    equals

---
+++ java-mid/java/src/lang/object/equals/UserV2.java
@@ -0,0 +1,27 @@
+package lang.object.equals;
+
+import java.util.Objects;
+
+public class UserV2 {
+
+    private String id;
+
+    public UserV2(String id) {
+        this.id = id;
+    }
+
+/*
+    @Override
+    public boolean equals(Object obj) {
+        UserV2 user = (UserV2) obj;
+        return id.equals(user.id);
+    }
+*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserV2 userV2)) return false;
+        return Objects.equals(id, userV2.id);
+    }
+}
