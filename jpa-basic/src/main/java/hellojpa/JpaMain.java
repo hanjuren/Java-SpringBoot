0d3dbb9b7b9d788c66351649805a340a150dd4e9
hanjuren <juren528@gmail.com>
2024-11-02 14:00:10 +0900

    proxy

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 
@@ -14,18 +15,25 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            Member member = Member.builder()
-                    .name("member1")
-                    .createdBy("kim")
-                    .createdDate(LocalDateTime.now())
-                    .lastModifiedDate(LocalDateTime.now())
-                    .lastModifiedBy("kim")
-                    .build();
-            em.persist(member);
+            Member reference = em.getReference(Member.class, 1L);
+            System.out.println("reference = " + reference.getClass());
 
-            transaction.commit();
+            Member findMember1 = em.find(Member.class, 1L);
+            System.out.println("findMember1 = " + findMember1.getClass());
+
+            em.clear();
+
+            Member findMember2 = em.getReference(Member.class, 1L);
+            System.out.println("findMember2 = " + findMember2.getClass());
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(findMember2));
+            Hibernate.initialize(findMember2);
+            System.out.println("findMember2 = " + findMember2.getClass());
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(findMember2));
+//            em.clear();
+//            System.out.println("findMember2 = " + findMember2.getName()); // org.hibernate.LazyInitializationException
         } catch (Exception e) {
             transaction.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
